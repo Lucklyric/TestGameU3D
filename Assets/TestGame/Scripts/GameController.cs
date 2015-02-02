1ed79ec2a22d93a075b332d4b03b1337ac31666d
Alvin Sun <lucklyric@gmail.com>
2015-02-02 16:06:17 -0700

    Some Changes ON GameController

---
+++ Assets/TestGame/Scripts/GameController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController: MonoBehaviour
+{
+
+
+		private static GameController _instance = null;
+	
+		public static GameController SharedInstance {
+				get {
+						if (_instance == null) {
+								_instance = GameObject.FindObjectOfType (typeof(GameController)) as GameController;
+						}
+			
+						return _instance;
+				}
+		}
+	
+		void Awake ()
+		{      
+				_instance = this; 
+		}
+
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+
+		public void BackToHomePage ()
+		{
+				Application.LoadLevel (0);
+		}
+
+		public void OnRing()
+		{
+				Debug.Log("onRing");
+		}
+}
